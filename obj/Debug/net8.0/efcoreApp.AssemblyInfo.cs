34b1483ad96dbf102a45f6f921b2b01e4142cc81
uo <uozalp@gmail.com>
2024-04-23 18:50:07 +0300

    deneme

--- obj/Debug/net8.0/efcoreApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/efcoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("efcoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66f9a30926c5ef63411311e76c7bddf0771d4469")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+059efe87f913983fcc4f740d21d9637dfdd52de1")]
 [assembly: System.Reflection.AssemblyProductAttribute("efcoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("efcoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
